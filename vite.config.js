a232ca72e4e82a407e5039e349ae434fba95adbb
jeimcg <mcgilljei@gmail.com>
2025-04-01 13:17:11 -0400

    fixed vite config for gh deploy

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import ghPages from 'vite-plugin-gh-pages';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/thepilotshow-site/',  // 👈 your repo name here
   plugins: [react()],
-  base: "/thepilotshow-site/",
-});
+})
