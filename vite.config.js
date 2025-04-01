8fa879608c79b8378b174c6d2213ccccb50c87cb
jeimcg <mcgilljei@gmail.com>
2025-04-01 13:59:07 -0400

    nvm on deployment for now lol

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/thepilotshow-site/',  // 👈 your repo name here
   plugins: [react()],
 })
