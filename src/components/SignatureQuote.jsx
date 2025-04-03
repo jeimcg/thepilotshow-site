3e7a8aa24597393097a14082d69159ab14fa3bc8
jeimcg <mcgilljei@gmail.com>
2025-04-03 17:00:24 -0400

    ...

--- src/components/SignatureQuote.jsx
+++ src/components/SignatureQuote.jsx
@@ -21,7 +21,7 @@ export default function SignatureQuote() {
         Getting Thru it.”
       </p>
       <img
-        src={`${import.meta.env.BASE_URL}dakotas-sig.svg`}
+        src="./dakotas-sig.svg"
         alt="Dakota's signature"
         className="h-24 opacity-70 dark:invert"
       />
