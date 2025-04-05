c880b130b6932fc535d3fef83eb9d7cfffc8042f
jeimcg <mcgilljei@gmail.com>
2025-04-05 18:11:40 -0400

    added music info

--- src/pages/Music.jsx
+++ src/pages/Music.jsx
@@ -4,40 +4,39 @@ const Music = () => {
   const tracks = [
     {
       title: 'Joanna (Ft. Kole)',
-      cover: '/images/track1.jpg', // or use a URL for now
+      cover: '	https://i1.sndcdn.com/artworks-000235940690-mqeh3z-t1080x1080.jpg', // using url for img
       link: 'https://on.soundcloud.com/zgZKJQWj8Evx4fdg8',
       platform: 'SoundCloud',
       description: '',
     },
     {
-      title: 'Love Ft. (Rock Ali)',
-      cover: '/images/track1.jpg', // or use a URL for now
+      title: 'Love (Ft. Rock Ali)',
+      cover: 'https://i1.sndcdn.com/artworks-000122180514-s7q3dm-t1080x1080.jpg',
       link: 'https://on.soundcloud.com/fzVwebrQZWdSL41N7',
       platform: 'SoundCloud',
       description: '',
     },
     {
       title: 'Purple Rain Freestyle',
-      cover: '/images/track1.jpg', // or use a URL for now
+      cover: 'https://i1.sndcdn.com/artworks-000205348954-p6b8ya-t1080x1080.jpg', 
       link: 'https://on.soundcloud.com/Wk3gqS6yyVoWo2vc6',
       platform: 'SoundCloud',
       description: '',
     },
     {
       title: 'Mixtape 3: 2016 The Summer Mixtape',
-      cover: '/images/epcover.jpg',
+      cover: 'https://i1.sndcdn.com/artworks-000168996979-z4n1hb-t1080x1080.jpg',
       link: 'https://on.soundcloud.com/yvWpTQE8ZFe3YuD77',
       platform: 'SoundCloud',
       description: '',
     },
     {
       title: 'FTL Tape 2017-2018',
-      cover: '/images/epcover.jpg',
+      cover: 'https://i1.sndcdn.com/artworks-000281722526-5a9yx2-t1080x1080.jpg',
       link: 'https://on.soundcloud.com/4vX1Y361ztydA5LL7',
       platform: 'SoundCloud',
       description: '',
     },
-    // Add more tracks here
   ];
 
   return (
