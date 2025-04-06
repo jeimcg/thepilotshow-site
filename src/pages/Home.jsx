234ddd8fff0717b49812cbfb64ce84fe5b063746
jeimcg <mcgilljei@gmail.com>
2025-04-05 22:29:20 -0400

    build error test

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react'
 import Header from '../components/Header'
-import RunWayStripe from '../components/RunWayStripe'
+import RunwayStripe from '../components/RunwayStripe'
 import FooterQuote from '../components/FooterQuote'
 import PageWrapper from '../components/PageWrapper'
 import '../animations/plane.css'
@@ -39,7 +39,7 @@ const Home = () => {
   return (
     <section className="relative min-h-[calc(100vh-4rem)] flex flex-col items-center text-center pb-20 overflow-hidden">
       {/* 🔥 Runway Stripe */}
-      <RunWayStripe />
+      <RunwayStripe />
 
       <PageWrapper>
         {/* Plane animation overlay */}
